376c8446d802526f4456e9a284cb7a557e629868
Charles Clément <caratorn@gmail.com>
2009-01-04 19:08:44 +0100

    Add 2009 Copyright

--- src/qwo.c
+++ src/qwo.c
@@ -1,7 +1,7 @@
 /*
  *  qwo: quikwriting for OpenMoko
  *
- *  Copyright (C) 2008 Charles Clement caratorn _at_ gmail.com
+ *  Copyright (C) 2008, 2009 Charles Clement caratorn _at_ gmail.com
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
